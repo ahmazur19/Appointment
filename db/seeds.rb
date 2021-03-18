34fcac9f76b30f5e679a2195584fee60e32b4ee7
Adam Mazur <adammazue@gmail.com>
2021-03-18 15:17:49 -0600

    init commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ 
+Appointment.destroy_all
+Patient.destroy_all
+Doctor.destroy_all
+
+10.times do |i|
+
+	doctor = Doctor.create(name: Faker::Name.name)
+	patient = Patient.create(name: Faker::Name.name)
+
+	5.times do |i|
+
+	Appointment.create(doctor_id:doctor.id, patient_id:patient.id)
+	end
+end
+
